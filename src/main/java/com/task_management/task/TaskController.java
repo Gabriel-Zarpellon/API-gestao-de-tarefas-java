873e443deb982db8a2264246a95e112fdb89af6d
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-12-10 16:25:10 -0300

    feat: add create task method

--- src/main/java/com/task_management/task/TaskController.java
+++ src/main/java/com/task_management/task/TaskController.java
@@ -1,5 +1,21 @@
 package com.task_management.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class TaskController {
-    
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping("/tasks")
+    public TaskEntity create(@RequestBody TaskEntity data) {
+        TaskEntity response = taskService.create(data);
+
+        return response;
+    }
 }
--- src/main/java/com/task_management/task/TaskService.java
+++ src/main/java/com/task_management/task/TaskService.java
@@ -1,5 +1,14 @@
 package com.task_management.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaskService {
-    
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public TaskEntity create(TaskEntity data) {
+        return taskRepository.save(data);
+    }
 }

358ad64a29daee2b09574a6a0e8a7805fe9e4b99
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-12-10 14:07:54 -0300

    feat: define task entity attributes

--- src/main/java/com/task_management/task/TaskEntity.java
+++ src/main/java/com/task_management/task/TaskEntity.java
@@ -1,5 +1,23 @@
 package com.task_management.task;
 
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity(name = "tasks")
 public class TaskEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private String description;
 }

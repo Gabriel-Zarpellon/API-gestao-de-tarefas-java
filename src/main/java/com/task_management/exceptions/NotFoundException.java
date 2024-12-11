689054f61d26e01c7b6bfd19776d51571d3b2f0d
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-12-11 15:28:47 -0300

    feat: create not found exception

--- src/main/java/com/task_management/exceptions/NotFoundException.java
+++ src/main/java/com/task_management/exceptions/NotFoundException.java
@@ -1,5 +1,11 @@
 package com.task_management.exceptions;
 
-public class NotFoundException {
-    
+public class NotFoundException extends Exception {
+    public NotFoundException() {
+        super("Task not found.");
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
 }

b710a56a749d7349aa2dc4ba167010e882a5f5a4
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-12-11 13:31:11 -0300

    feat: create findByStatus method

--- src/main/java/com/task_management/task/TaskRepository.java
+++ src/main/java/com/task_management/task/TaskRepository.java
@@ -1,8 +1,11 @@
 package com.task_management.task;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<TaskEntity, UUID> {
+    List<TaskEntity> findByStatus(Optional<String> status);
 
 }

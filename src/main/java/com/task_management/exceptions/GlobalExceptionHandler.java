267ea4eca06994a568c5cc700d5edd22e8f872f2
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-12-11 15:29:55 -0300

    feat: create global exception handler and add error message and not found exception handlers

--- src/main/java/com/task_management/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/task_management/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,43 @@
 package com.task_management.exceptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.MessageSource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
-    
+    private MessageSource messageSource;
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<ErrorMessageDTO> dto = new ArrayList<>();
+
+        e.getBindingResult().getFieldErrors().forEach(err -> {
+            String message = err.getDefaultMessage();
+            ErrorMessageDTO error = new ErrorMessageDTO();
+            error.setError(message);
+            dto.add(error);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dto);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(NotFoundException e) {
+        ErrorMessageDTO error = new ErrorMessageDTO();
+        error.setError(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    public GlobalExceptionHandler(MessageSource message) {
+        this.messageSource = message;
+    }
 }

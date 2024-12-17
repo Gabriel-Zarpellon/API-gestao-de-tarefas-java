9cde244dcf196562c0c016d2fa645494ef271425
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-12-17 13:44:11 -0300

    feat: create user entity

---
+++ src/main/java/com/task_management/user/UserEntity.java
@@ -0,0 +1,34 @@
+package com.task_management.user;
+
+import java.util.List;
+
+import com.task_management.task.TaskEntity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Data
+@Entity(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<TaskEntity> tasks;
+}
